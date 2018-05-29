63a01c4240a0c08b3acd9b87561a46a01343c42b
MSobiecki <michalsobiecki@yahoo.com>
2018-05-29 15:32:14 +0200

    Save

--- szablon/src/main.js
+++ szablon/src/main.js
@@ -1,5 +1,12 @@
 /* eslint-env browser */
 import $ from 'jquery';
+import 'babel-polyfill';
+
+import Favicon from './assets/favicon/favicon.ico';
+import FaviconXML from './assets/favicon/browserconfig.xml';
+import FaviconManifest from './assets/favicon/manifest.json';
+let requireContext = require.context("./assets/favicon", true, /^\.\/.*\.png$/);
+requireContext.keys().map(requireContext);
 
 import Loader from './commons/loader/loader';
 
